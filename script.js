07f6ff91f1355b63ebaf794dc2cff856adc70cfa
Jason Hu <jsnhu.os@gmail.com>
2024-01-27 23:34:15 -0800

    add hovering etch functionality

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 gridContainer = document.querySelector('.grid-container');
 
-for (i=0;i<16;i++) {
+for (i=0;i<256;i++) {
     const newGrid = document.createElement('div');
     newGrid.classList.toggle('square');
 
     gridContainer.appendChild(newGrid);
 }
+
+squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', ()=>{
+        square.style.backgroundColor = "black";
+    });
+});
