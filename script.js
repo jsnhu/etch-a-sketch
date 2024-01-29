68acf4ee532e4a5ca3d85b6a7ae83e572adb1adc
Jason Hu <jsnhu.os@gmail.com>
2024-01-29 11:06:29 -0800

    make squares tile height with no extra

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const gridContainer = document.querySelector('.grid-container');
 
 const SQUARE_SIZE = 30;
 
+gridContainer.style.width = screen.availWidth;
+gridContainer.style.height = screen.availHeight;
+
 const numSquaresWidth = Math.floor(screen.availWidth / SQUARE_SIZE);
-const numSquaresHeight = Math.floor(screen.availHeight / SQUARE_SIZE);
+// - 0 makes a scroll bar. - 3 seems to perfectly fit my screen on chrome
+const numSquaresHeight = Math.floor(screen.availHeight / SQUARE_SIZE) - 3;
 
 for (i=0;i<numSquaresHeight*numSquaresWidth;i++) {
     const newGrid = document.createElement('div');
